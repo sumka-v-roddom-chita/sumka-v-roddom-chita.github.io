cc3394c62ee9a7183462a446dc963e1a1d1ed94d
Baira Santaeva <bsantaeva@microsoft.com>
2021-05-30 13:55:10 +0300

    add content

---
+++ src/components/contact.jsx
@@ -0,0 +1,95 @@
+import { useState } from 'react'
+import emailjs from 'emailjs-com'
+
+const initialState = {
+  name: '',
+  email: '',
+  message: '',
+}
+
+export const Contact = (props) => {
+  const [{ name, email, message }, setState] = useState(initialState)
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setState((prevState) => ({ ...prevState, [name]: value }))
+  }
+
+  const clearState = () => setState({ ...initialState })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    
+    emailjs
+      .sendForm(
+        'YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID'
+      )
+      .then(
+        (result) => {
+          console.log(result.text)
+          clearState()
+        },
+        (error) => {
+          console.log(error.text)
+        }
+      )
+  }
+  return (
+    <div>
+      <div id='contact'>
+        <div className='container'>
+          <div className='col-md-3 col-md-offset-1 contact-info'>
+            <div className='contact-item'>
+              <h3>Contact Info</h3>
+              <p>
+                <span>
+                  <i className='fa fa-map-marker'></i> Address
+                </span>
+                {props.data ? props.data.address : 'загрузка'}
+              </p>
+            </div>
+            <div className='contact-item'>
+              <p>
+                <span>
+                  <i className='fa fa-phone'></i> Phone
+                </span>{' '}
+                {props.data ? props.data.phone : 'загрузка'}
+              </p>
+            </div>
+            <div className='contact-item'>
+              <p>
+                <span>
+                  <i className='fa fa-envelope-o'></i> Email
+                </span>{' '}
+                {props.data ? props.data.email : 'загрузка'}
+              </p>
+            </div>
+          </div>
+          <div className='col-md-12'>
+            <div className='row'>
+              <div className='social'>
+                <ul>
+                  <li>
+                    <a href={props.data ? props.data.facebook : '/'}>
+                      <i className='fa fa-facebook'></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href={props.data ? props.data.twitter : '/'}>
+                      <i className='fa fa-twitter'></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href={props.data ? props.data.instargam : '/'}>
+                      <i className='fa fa-instagram'></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
